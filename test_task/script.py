8164e91e16478d41205bd9b5906cb21c5d2d0ce9
SmolskayaNastya <smolsnastya9696@gmail.com>
2017-07-04 10:20:15 +0200

    test_task

---
+++ test_task/script.py
@@ -0,0 +1,20 @@
+﻿try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+import pandas as pd
+from bs4 import BeautifulSoup
+links=pd.read_csv('wikipedia_links.csv',header=None)[0]
+urls=[]
+for i in range(len(links)):    
+    page = urlopen(links[i]) 
+    
+    #Парсим страницу с помощью BeautifulSoup
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    #Получаем со страницы все теги с классом small
+    urls_tag = soup.findAll(attrs={"rel":"nofollow"})
+    
+    urls.append(urls_tag[0]["href"])
+new_links=pd.concat([pd.DataFrame(list(links),columns=['wikipedia_page']),pd.DataFrame(urls,columns=['website'])],axis=1)
+new_links.to_csv('wikipedia_answers.csv',index=None)
